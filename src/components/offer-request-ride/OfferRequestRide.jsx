bbef708d8be2baa0af19ee14b71cdbdc3c1dc043
Javier <javivasv@hotmail.com>
2020-02-01 11:49:21 -0400

    Mensajes en botones

--- src/components/offer-request-ride/OfferRequestRide.jsx
+++ src/components/offer-request-ride/OfferRequestRide.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import "./OfferRequestRide.css";
+import autostop from "assets/images/autostop.jpg";
 
 class OfferRequestRide extends Component {
   render() {
     return (
       <div className="OfferRequestRide">
-        <div className="OfferRequest">
-          <button className="RequestButton"></button>
-          <button className="OfferButton"></button>
-        </div>
         <div className="RideLog" style={{ color: "white", fontWeight: "bold" }}>
           *PATROCINADORES/HISTORIAL*
+        </div>
+        <div className="OfferRequest">
+          <button className="RequestButton">
+            <span className="Mensaje">
+              Solicita una cola para ir a la Universidad o para salir de ella.
+            </span>
+          </button>
+          <button className="OfferButton">
+            <span className="Mensaje">
+              Brinda la cola a un compañero, profesor o empleado para ir o salir
+              de la Universidad.
+            </span>
+          </button>
         </div>
       </div>
     );
--- src/containers/HomePage.jsx
+++ src/containers/HomePage.jsx
@@ -3,6 +3,10 @@ import React, { Component } from "react";
 //import FormLogin from "../components/form-login/FormLogin";
 import "assets/css/HomePage.css";
 import logo from "assets/images/logo.png";
+import list from "assets/images/list.svg";
+import houseFill from "assets/images/houseFill.svg";
+import personFill from "assets/images/personFill.svg";
+import infoFill from "assets/images/infoFill.svg";
 import profilePicture from "assets/images/profilePicture.jpg";
 import OfferRequestRide from "../components/offer-request-ride/OfferRequestRide";
 
@@ -11,6 +15,7 @@ class HomePage extends Component {
     return (
       <div className="HomePage">
         <nav className="Top">
+          <img className="OptionList" src={list} alt="OptionList" />
           <img className="Logo" src={logo} alt="Logo" />
         </nav>
         <div class="container-fluid">
@@ -27,14 +32,31 @@ class HomePage extends Component {
                   <p className="Carnet">Carnet</p>
                 </div>
                 <ul class="nav flex-column">
-                  <li class="Options">
-                    <a style={{ color: "black", fontWeight: "bold" }}>Inicio</a>
+                  <li className="Options">
+                    <img
+                      className="HouseFill"
+                      src={houseFill}
+                      alt="HouseFill"
+                    />
+                    <span style={{ color: "black", fontWeight: "bold" }}>
+                      Inicio
+                    </span>
                   </li>
-                  <li class="Options">
-                    <a style={{ color: "black", fontWeight: "bold" }}>Perfil</a>
+                  <li className="Options">
+                    <img
+                      className="PersonFill"
+                      src={personFill}
+                      alt="PersonFill"
+                    />
+                    <span style={{ color: "black", fontWeight: "bold" }}>
+                      Perfil
+                    </span>
                   </li>
-                  <li class="Options">
-                    <a style={{ color: "black", fontWeight: "bold" }}>Ayuda</a>
+                  <li className="Options">
+                    <img className="InfoFill" src={infoFill} alt="InfoFill" />
+                    <span style={{ color: "black", fontWeight: "bold" }}>
+                      Ayuda
+                    </span>
                   </li>
                 </ul>
               </div>
@@ -52,176 +74,101 @@ class HomePage extends Component {
 
 export default HomePage;
 
-/*<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="#">Sign out</a>
-        </li>
-      </ul>
-    </nav>
-*/
-
 /*
-<nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link active" href="#">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                  Dashboard <span class="sr-only">(current)</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-*/
-
-/*
-<a href="#" class="img logo rounded-circle mb-5" style="background-image: url(images/logo.jpg);"></a>*/
-
-/*
-<div class="container-fluid">
-  <div class="row">
-    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-      <div class="sidebar-sticky">
-        <ul class="nav flex-column">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">
-              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-              <polyline points="9 22 9 12 15 12 15 22"></polyline>
-              Dashboard <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path>
-              <polyline points="13 2 13 9 20 9"></polyline>
-              Orders
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <circle cx="9" cy="21" r="1"></circle>
-              <circle cx="20" cy="21" r="1"></circle>
-              <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
-              Products
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-              <circle cx="9" cy="7" r="4"></circle>
-              <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
-              <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-              Customers
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <line x1="18" y1="20" x2="18" y2="10"></line>
-              <line x1="12" y1="20" x2="12" y2="4"></line>
-              <line x1="6" y1="20" x2="6" y2="14"></line>
-              Reports
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
-              <polyline points="2 17 12 22 22 17"></polyline>
-              <polyline points="2 12 12 17 22 12"></polyline>
-              Integrations
-            </a>
-          </li>
-        </ul>
-
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>Saved reports</span>
-          <a class="d-flex align-items-center text-muted" href="#">
-            <circle cx="12" cy="12" r="10"></circle>
-            <line x1="12" y1="8" x2="12" y2="16"></line>
-            <line x1="8" y1="12" x2="16" y2="12"></line>
-          </a>
-        </h6>
-        <ul class="nav flex-column mb-2">
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line x1="16" y1="13" x2="8" y2="13"></line>
-              <line x1="16" y1="17" x2="8" y2="17"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-              Current month
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line x1="16" y1="13" x2="8" y2="13"></line>
-              <line x1="16" y1="17" x2="8" y2="17"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-              Last quarter
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line x1="16" y1="13" x2="8" y2="13"></line>
-              <line x1="16" y1="17" x2="8" y2="17"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-              Social engagement
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
-              <polyline points="14 2 14 8 20 8"></polyline>
-              <line x1="16" y1="13" x2="8" y2="13"></line>
-              <line x1="16" y1="17" x2="8" y2="17"></line>
-              <polyline points="10 9 9 9 8 9"></polyline>
-              Year-end sale
-            </a>
-          </li>
-        </ul>
+<nav class="sidenav---_u0En collapsed---2IIb7">
+  <button role="button" class="sidenav-toggle---23_Hl" aria-expanded="false">
+    <span class="icon-bar---2jamJ"></span>
+    <span class="icon-bar---2jamJ"></span>
+    <span class="icon-bar---2jamJ"></span>
+  </button>
+  <div role="menu" class="sidenav-nav---uvKqK">
+    <div
+      role="presentation"
+      class="sidenav-navitem---3r9ER selected---91VwC highlighted---1vG8Y"
+    >
+      <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+        <div class="navicon---d-E_g">
+          <i
+            class="fa fa-fw fa-home"
+            style="font-size: 1.75em; vertical-align: middle;"
+          ></i>
+        </div>
+        <div title="Home" class="navtext---2Ylza" style="padding-right: 32px;">
+          Home
+        </div>
       </div>
-    </nav>
-
-    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-      <div
-        class="chartjs-size-monitor"
-        style="position: absolute; left: 0px; top: 0px; right: 0px; bottom: 0px; overflow: hidden; pointer-events: none; visibility: hidden; z-index: -1;"
-      >
-        <div
-          class="chartjs-size-monitor-expand"
-          style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"
-        >
-          <div style="position:absolute;width:1000000px;height:1000000px;left:0;top:0"></div>
+    </div>
+    <div role="presentation" class="sidenav-navitem---3r9ER">
+      <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+        <div class="navicon---d-E_g">
+          <i
+            class="fa fa-fw fa-line-chart"
+            style="font-size: 1.75em; vertical-align: middle;"
+          ></i>
         </div>
         <div
-          class="chartjs-size-monitor-shrink"
-          style="position:absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1;"
+          title="Devices"
+          class="navtext---2Ylza"
+          style="padding-right: 32px;"
         >
-          <div style="position:absolute;width:200%;height:200%;left:0; top:0"></div>
+          Devices
         </div>
       </div>
-      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dashboard</h1>
-        <div class="btn-toolbar mb-2 mb-md-0">
-          <div class="btn-group mr-2">
-            <button class="btn btn-sm btn-outline-secondary">Share</button>
-            <button class="btn btn-sm btn-outline-secondary">Export</button>
+    </div>
+    <div role="presentation" class="sidenav-navitem---3r9ER">
+      <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+        <div class="navicon---d-E_g">
+          <i
+            class="fa fa-fw fa-list-alt"
+            style="font-size: 1.75em; vertical-align: middle;"
+          ></i>
+        </div>
+        <div
+          title="Reports"
+          class="navtext---2Ylza"
+          style="padding-right: 32px;"
+        >
+          Reports
+        </div>
+      </div>
+    </div>
+    <div role="presentation" class="sidenav-navitem---3r9ER expandable---2ZpGQ">
+      <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+        <div class="navicon---d-E_g">
+          <i
+            class="fa fa-fw fa-cogs"
+            style="font-size: 1.5em; vertical-align: middle;"
+          ></i>
+        </div>
+        <div
+          title="Settings"
+          class="navtext---2Ylza"
+          style="padding-right: 32px;"
+        >
+          Settings
+        </div>
+      </div>
+      <div role="menu" class="sidenav-subnav---3Hr7K">
+        <div role="heading" class="sidenav-subnavitem---3Rbfo">
+          Settings
+        </div>
+        <div role="presentation" class="sidenav-subnavitem---3Rbfo">
+          <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+            <div title="Policies" class="navtext---2Ylza">
+              Policies
+            </div>
           </div>
-          <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
-            This week
-          </button>
+        </div>
+        <div role="presentation" class="sidenav-subnavitem---3Rbfo">
+          <div class="navitem---3LFxv" role="menuitem" tabindex="-1">
+            <div title="Network" class="navtext---2Ylza">
+              Network
+            </div>
+          </div>
         </div>
       </div>
-
-      <h2>Section title</h2>
-    </main>
+    </div>
   </div>
-</div>
+</nav>
+
 */
