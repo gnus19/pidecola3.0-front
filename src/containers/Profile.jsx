56fbc67f3f43fb3ac9b036228a3826a4affe6991
Angel Morante <morante413@gmail.com>
2020-02-24 12:54:12 -0400

    mejores perfil

--- src/containers/Profile.jsx
+++ src/containers/Profile.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import "assets/css/Profile.css";
+import profilePicture from "assets/images/profilePicture.jpg"
+import usercar from "assets/images/user-car.png"
 import InputPC from 'components/input-pc/InputPC'
+import ImgContainer from 'components/user-img/ImgContainer'
 
 class Profile extends Component {
 	render() {
 		return (
 			<div className="Profile">
 				<div className="Section-Profile-Left">
-
+					<div className="child1">
+						<ImgContainer 
+							src={profilePicture}
+							alt="Image Profile"
+						/>
+					</div>
+					<div className="child2">
+						<div className="ContentCar">
+							<p>Vehículo 1</p>
+							<ImgContainer 
+								src={usercar}
+								alt="Image Profile"
+								height="130px"
+								width="130px"
+							/>
+						</div>
+					</div>
 				</div>
 				<div className="Section-Profile-Rigth">
 					<InputPC
