f6baff85c1bfb63755386c298836b716b9cbbbea
Angel <morante413@gmail.com>
2020-03-21 19:53:53 -0400

    del

--- src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = app => {
-  app.use(createProxyMiddleware("/*", { target: "https://pidecolausb.herokuapp.com/" }))
-}
