9e9e9d0e3a2edaafd32b9674932adb9ab7643362
gnus19 <andrecorcuera007@gmail.com>
2020-03-27 23:37:39 -0400

    componentDidMount equivalent in functions

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,17 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import { infoProfile } from "services/userServices";
 import logo from "assets/images/logo.png";
 import list from "assets/images/list.svg";
 import ImgContainer from "components/userImg/ImgContainer";
 import profilePicture from "assets/images/profilePicture.jpg";
-
-componentDidMount() {
-  document.getElementById("profileOptions").style.background = "#1e2172";
-    document.getElementById("profileOptions").style.color = "#fff";
-    document.getElementById("helpOptions").style.background = "#1e2172";
-    document.getElementById("helpOptions").style.color = "#fff";
-};
 
 const openNav = () => {
   document.getElementById("Sidebar").style.width = "50%";
@@ -93,103 +86,112 @@ infoProfile()
     console.log("Catch", error);
   });
 
-const Main = ({ children }) => (
-  <div className="HomePage">
-    <div id="Content-Prin">
-      <nav className="Top">
-        <div className="TopOption">
-          <button className="OptionButton" onClick={openNav}>
-            <img className="OptionList" src={list} alt="OptionList" />
-          </button>
-        </div>
-        <div className="TopLogo">
-          <NavLink
-            to="/home"
-            onClick={() => {
-              changeColor("homeOptions");
-            }}
-          >
-            <img className="HomeLogo" src={logo} alt="HomeLogo" />
-          </NavLink>
-        </div>
-      </nav>
-      <div id="Content-Sec">
-        <nav className="Sidebar" id="Sidebar">
-          <a
-            href="javascript:void(0)"
-            className="HideOption"
-            onClick={closeNav}
-          >
-            ×
-          </a>
-          <div className="sidebar-sticky">
-            <div className="Info">
-              <ImgContainer
-                src={localStorage.getItem("profilePic")}
-                alt="profilePicture pidecola"
-                height="65%"
-                width="65%"
-              />
-              <p className="Username">
-                {localStorage.getItem("firstName")}{" "}
-                {localStorage.getItem("lastName")}
-              </p>
-              <p className="Carnet">{localStorage.getItem("carnet")}</p>
-            </div>
-            <ul className="nav flex-column">
-              <NavLink
-                to="/home"
-                onClick={() => {
-                  changeColor("homeOptions");
-                }}
-              >
-                <li className="Options" id="homeOptions">
-                  <i className="material-icons">home</i>
-                  <span>Inicio</span>
-                </li>
-              </NavLink>
-              <NavLink
-                to="/profile"
-                onClick={() => {
-                  changeColor("profileOptions");
-                }}
-              >
-                <li className="Options" id="profileOptions">
-                  <i className="material-icons">account_circle</i>
-                  <span>Perfil</span>
-                </li>
-              </NavLink>
-              <NavLink
-                to="/help"
-                onClick={() => {
-                  changeColor("helpOptions");
-                }}
-              >
-                <li className="Options" id="helpOptions">
-                  <i className="material-icons">help</i>
-                  <span>Ayuda</span>
-                </li>
-              </NavLink>
-              <NavLink
-                to="/login"
-                onClick={() => {
-                  removeLocalStorage("helpOptions");
-                }}
-              >
-                <li className="Options" id="helpOptions">
-                  <i className="material-icons">exit_to_app</i>
-                  <span>Salir</span>
-                </li>
-              </NavLink>
-            </ul>
+const Main = ({ children }) => {
+  useEffect(() => {
+    document.getElementById("profileOptions").style.background = "#1e2172";
+    document.getElementById("profileOptions").style.color = "#fff";
+    document.getElementById("helpOptions").style.background = "#1e2172";
+    document.getElementById("helpOptions").style.color = "#fff";
+  })
+  return (
+    <div className="HomePage">
+      <div id="Content-Prin">
+        <nav className="Top">
+          <div className="TopOption">
+            <button className="OptionButton" onClick={openNav}>
+              <img className="OptionList" src={list} alt="OptionList" />
+            </button>
+          </div>
+          <div className="TopLogo">
+            <NavLink
+              to="/home"
+              onClick={() => {
+                changeColor("homeOptions");
+              }}
+            >
+              <img className="HomeLogo" src={logo} alt="HomeLogo" />
+            </NavLink>
           </div>
         </nav>
-        <div className="Main" id="main" onClick={closeNav}>
-          {children}
+        <div id="Content-Sec">
+          <nav className="Sidebar" id="Sidebar">
+            <a
+              href="javascript:void(0)"
+              className="HideOption"
+              onClick={closeNav}
+            >
+              ×
+            </a>
+            <div className="sidebar-sticky">
+              <div className="Info">
+                <ImgContainer
+                  src={localStorage.getItem("profilePic")}
+                  alt="profilePicture pidecola"
+                  height="65%"
+                  width="65%"
+                />
+                <p className="Username">
+                  {localStorage.getItem("firstName")}{" "}
+                  {localStorage.getItem("lastName")}
+                </p>
+                <p className="Carnet">{localStorage.getItem("carnet")}</p>
+              </div>
+              <ul className="nav flex-column">
+                <NavLink
+                  to="/home"
+                  onClick={() => {
+                    changeColor("homeOptions");
+                  }}
+                >
+                  <li className="Options" id="homeOptions">
+                    <i className="material-icons">home</i>
+                    <span>Inicio</span>
+                  </li>
+                </NavLink>
+                <NavLink
+                  to="/profile"
+                  onClick={() => {
+                    changeColor("profileOptions");
+                  }}
+                >
+                  <li className="Options" id="profileOptions">
+                    <i className="material-icons">account_circle</i>
+                    <span>Perfil</span>
+                  </li>
+                </NavLink>
+                <NavLink
+                  to="/help"
+                  onClick={() => {
+                    changeColor("helpOptions");
+                  }}
+                >
+                  <li className="Options" id="helpOptions">
+                    <i className="material-icons">help</i>
+                    <span>Ayuda</span>
+                  </li>
+                </NavLink>
+                <NavLink
+                  to="/login"
+                  onClick={() => {
+                    removeLocalStorage("helpOptions");
+                  }}
+                >
+                  <li className="Options" id="helpOptions">
+                    <i className="material-icons">exit_to_app</i>
+                    <span>Salir</span>
+                  </li>
+                </NavLink>
+              </ul>
+            </div>
+          </nav>
+          <div className="Main" id="main" onClick={closeNav}>
+            {children}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+}
+
 
 export default Main;
