d54a5bfcb3e9c89c3ad7923ac627879a3a5db5e4
Javier <javivasv@hotmail.com>
2020-04-07 18:33:30 -0400

    Vista de Ayuda

--- src/containers/Help.jsx
+++ src/containers/Help.jsx
@@ -10,7 +10,7 @@ class Help extends Component {
     this.state = {};
   }
 
-  goTo = event => {
+  goTo = (event) => {
     event.preventDefault();
     const id = event.target.id;
     if (id === "twitter") {
@@ -28,34 +28,81 @@ class Help extends Component {
     return (
       <div className="Help">
         <div className="Terminos">
-          <div className="cartaAyuda" id="cartaTerminos"></div>
+          <div className="cartaAyuda" id="cartaTerminos">
+            <div className="helpTop" id="termsTop">
+              <span className="topText">TÉRMINOS Y CONDICIONES</span>
+            </div>
+          </div>
         </div>
         <div className="Personal">
-          <div className="cartaAyuda" id="cartaPersonal"></div>
+          <div className="cartaAyuda" id="cartaDevTeam">
+            <div className="helpTop">
+              <span className="topText">EQUIPO DE DESARROLLO</span>
+            </div>
+            <div className="personalContent">
+              <div className="team" id="devTeam">
+                <span className="infoAyuda">- ANDRÉ CORCUERA</span>
+                <span className="infoAyuda">- PEDRO MALDONADO</span>
+                <span className="infoAyuda">- FRANCISCO MÁRQUEZ</span>
+              </div>
+              <div className="team" id="devTeam">
+                <span className="infoAyuda">- ÁNGEL MORANTE</span>
+                <span className="infoAyuda">- JAVIER VIVAS</span>
+              </div>
+            </div>
+          </div>
+          <div className="cartaAyuda" id="cartaSupervisor">
+            <div className="helpTop">
+              <span className="topText">SUPERVISOR</span>
+            </div>
+            <div className="personalContent">
+              <div className="team" id="supervTeam">
+                <span className="infoAyuda">PROF. FERNANDO TORRE MORA</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="Links">
           <div className="cartaAyuda" id="cartaLinks">
-            <img
-              className="logoImage"
-              id="twitter"
-              src={twitter}
-              onClick={this.goTo}
-              alt='twitter'
+            <div className="helpTop">
+              <span className="topText">SIGUE A LA FCE EN SUS REDES</span>
+            </div>
+            <div className="linkLogos">
+              <img
+                className="logoImage"
+                id="twitter"
+                src={twitter}
+                onClick={this.goTo}
+                alt="twitter"
               />
-            <img
-              className="logoImage"
-              id="instagram"
-              src={instagram}
-              onClick={this.goTo}
-              alt='instagram'
+              <img
+                className="logoImage"
+                id="instagram"
+                src={instagram}
+                onClick={this.goTo}
+                alt="instagram"
               />
-            <img
-              className="logoImage"
-              id="telegram"
-              src={telegram}
-              onClick={this.goTo}
-              alt='telegram'
-            />
+              <img
+                className="logoImage"
+                id="telegram"
+                src={telegram}
+                onClick={this.goTo}
+                alt="telegram"
+              />
+            </div>
+          </div>
+          <div className="cartaAyuda" id="cartaContacto">
+            <div className="helpTop">
+              <span className="topText">CONTACTO</span>
+            </div>
+            <div className="contactoContent">
+              <div className="datos">
+                <span className="infoAyuda">NÚMERO - </span>
+              </div>
+              <div className="datos">
+                <span className="infoAyuda">CORREO - FCEUSB@USB.VE</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
