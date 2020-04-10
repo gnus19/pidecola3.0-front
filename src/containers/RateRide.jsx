59cf532a7c28c0459bc15ae0c877df0e3dd419dd
Javier <javivasv@hotmail.com>
2020-04-10 13:56:51 -0400

    Hover sobre like/dislike. Mensaje por haber calificado

--- src/containers/RateRide.jsx
+++ src/containers/RateRide.jsx
@@ -7,31 +7,85 @@ class RateRide extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      rated: false,
+      likeHovered: false,
+      dislikeHovered: false,
+    };
   }
 
+  onMouseEnterLike = (event) => {
+    this.setState({
+      likeHovered: true,
+    });
+  };
+
+  onMouseLeaveLike = (event) => {
+    this.setState({
+      likeHovered: false,
+    });
+  };
+
+  onMouseEnterDislike = (event) => {
+    this.setState({
+      dislikeHovered: true,
+    });
+  };
+
+  onMouseLeaveDislike = (event) => {
+    this.setState({
+      dislikeHovered: false,
+    });
+  };
+
+  rateRide = (event) => {
+    this.setState({
+      rated: true,
+    });
+  };
+
   render() {
+    const { likeHovered } = this.state;
+    const likeStyle = likeHovered ? { backgroundColor: "#4caf50" } : {};
+    const { dislikeHovered } = this.state;
+    const dislikeStyle = dislikeHovered ? { backgroundColor: "#d32f2f" } : {};
     return (
       <div className="rateRide">
         <div className="rateMessage">
-          <div className="carta" id="cartaMessage">
-            ¿CÓMO CALIFICARÍAS LA COLA QUE TE DIO *NOMBRE*?
-          </div>
+          {!this.state.rated ? (
+            <div className="carta" id="cartaMessage">
+              ¿CÓMO CALIFICARÍAS LA COLA QUE TE DIO *NOMBRE*?
+            </div>
+          ) : (
+            <div className="carta" id="cartaMessage">
+              ¡GRACIAS POR CALIFICAR LA COLA!
+            </div>
+          )}
         </div>
         <div className="rate">
           <div className="rateImages">
-            <div className="like">
-              <img className="likeImage" src={like} />
+            <div className="like" style={likeStyle}>
+              <img className="rateImage" src={like} />
             </div>
-            <div className="dislike">
-              <img className="likeImage" src={dislike} />
+            <div className="dislike" style={dislikeStyle}>
+              <img className="rateImage" src={dislike} />
             </div>
           </div>
           <div className="rateButtons">
-            <div className="likeButton">
+            <div
+              className="likeButton"
+              onMouseEnter={this.onMouseEnterLike}
+              onMouseLeave={this.onMouseLeaveLike}
+              onClick={this.rateRide}
+            >
               <span>LIKE</span>
             </div>
-            <div className="dislikeButton">
+            <div
+              className="dislikeButton"
+              onMouseEnter={this.onMouseEnterDislike}
+              onMouseLeave={this.onMouseLeaveDislike}
+              onClick={this.rateRide}
+            >
               <span>DISLIKE</span>
             </div>
           </div>
