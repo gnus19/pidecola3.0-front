25d3f59f3dd6381bc67d7ac94aa9d6e95122e554
ChuyB <bovea3359@gmail.com>
2024-02-22 17:25:13 -0400

    feat: adds register layout component

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen">
+      <Header />
+      {children}
+    </div>
+  );
+}
