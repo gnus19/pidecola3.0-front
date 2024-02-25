3c8e44969aa0f22532e3e54632fe6667f6fae579
ChuyB <bovea3359@gmail.com>
2024-02-24 21:40:39 -0400

    refactor: updates how the profile avatar is shown
    Now, the avatar has navigation to login, register and logout pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/logo.png";
-import ProfileAvatar from "./ProfileAvatar";
 import { useEffect, useState } from "react";
+import UserActions from "./UserActions";
 
 const Header = () => {
   const [isTop, setIsTop] = useState(true);
@@ -20,17 +20,20 @@ const Header = () => {
 
   return (
     <header
-      className={`z-50 sticky top-0 py-2 flex justify-between items-center w-screen bg-white dark:bg-gray-800 transition ease-in-out duration-300 ${
+      className={`z-50 sticky top-0 min-h-14 py-2 px-4 sm:px-10 flex justify-between sm:justify-end items-center w-screen bg-white dark:bg-gray-800 transition ease-in-out duration-300 ${
         isTop
           ? "border-b border-slate-200 dark:border-slate-600"
           : "shadow-md backdrop-blur-sm bg-gray-200/30 dark:bg-gray-900"
       }`}
     >
-      <Link href={"/"} className="mx-auto text-blue-600">
+      <Link
+        href={"/"}
+        className="sm:absolute w-fit right-0 left-0 sm:mx-auto text-blue-600"
+      >
         <Image src={logo} alt="Logo del PideCola" height={40} width={40} />
       </Link>
 
-      <ProfileAvatar className="absolute top-0 bottom-0 my-auto right-2 sm:right-4 md:right-16 xl:right-20" />
+      <UserActions />
     </header>
   );
 };
