16230a8ea5aa436ed04cdbdf9f36f4b9dd29a08d
ChuyB <bovea3359@gmail.com>
2024-02-26 18:05:26 -0400

    feat: session form data validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "@nextui-org/input";
 import { Button } from "@nextui-org/react";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/16/solid";
@@ -8,6 +8,103 @@ import Link from "next/link";
 import { loginUser } from "@/lib/actions/users";
 import { useFormState, useFormStatus } from "react-dom";
 import { useRouter } from "next/navigation";
+import { loginSchema } from "@/lib/validations/userSchema";
+
+const LoginForm = () => {
+  const router = useRouter();
+  const [isVisible, setIsVisible] = useState(false);
+  const [resMessage, dispatch] = useFormState(loginUser, undefined);
+  const [emailErrInfo, setEmailErrInfo] = useState("");
+  const [passErrInfo, setPassErrInfo] = useState("");
+
+  const submitAction = async (formData: FormData) => {
+    const data = Object.fromEntries(formData.entries());
+    const validationResponse = loginSchema.safeParse(data);
+    if (!validationResponse.success) {
+      const { fieldErrors } = validationResponse.error.formErrors;
+      const { email, password } = fieldErrors;
+      setEmailErrInfo(email?.[0] || "");
+      setPassErrInfo(password?.[0] || "");
+      return;
+    }
+    setEmailErrInfo("");
+    setPassErrInfo("");
+
+    dispatch(formData);
+  };
+
+  useEffect(() => {
+    if (resMessage?.status === 401) {
+      setEmailErrInfo("Ingresa tu correo USB");
+      setPassErrInfo(" ");
+    }
+  }, [resMessage]);
+
+  // Redirect if login was successful
+  if (resMessage?.status === 200) router.push("/profile");
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  return (
+    <div className="z-10 bg-slate-200/80 backdrop-blur-lg dark:bg-slate-800 md:shadow-lg dark:shadow-gray-900 md:rounded-xl p-6 w-full md:max-w-md md:border-solid md:border-2 md:border-slate-200 dark:md:border-slate-600">
+      <h1 className="mb-12 [text-shadow:_2px_2px_5px_rgb(0_0_0_/_25%)] font-black text-neutral-700 dark:text-slate-300 text-3xl text-center ">
+        Iniciar sesión
+      </h1>
+
+      {resMessage?.status === 401 ? (
+        <p className="text-danger text-center">
+          El usuario o la contraseña no son correctas
+        </p>
+      ) : (
+        ""
+      )}
+
+      <form className="flex flex-col items-center" action={submitAction}>
+        <Input
+          isRequired
+          type="email"
+          label="Email"
+          name="email"
+          labelPlacement="outside"
+          placeholder="xx-xxxxxxx@usb.ve"
+          description="Ingresa tu correo USB"
+          className="mb-8"
+          variant="faded"
+          isInvalid={emailErrInfo !== ""}
+          errorMessage={emailErrInfo}
+        />
+        <Input
+          isRequired
+          name="password"
+          label="Contraseña"
+          labelPlacement="outside"
+          placeholder="Ingresa tu contraseña"
+          endContent={
+            <button
+              className="focus:outline-none"
+              type="button"
+              onClick={toggleVisibility}
+            >
+              {isVisible ? <EyeIcon /> : <EyeSlashIcon />}
+            </button>
+          }
+          type={isVisible ? "text" : "password"}
+          variant="faded"
+          isInvalid={passErrInfo !== ""}
+          errorMessage={passErrInfo}
+        />
+
+        <LoginButton />
+      </form>
+      <p className="mt-4 text-sm text-gray-600 text-center">
+        ¿Aún no estás registrado?{" "}
+        <Link href="/register" className="text-blue-500">
+          Regístrate
+        </Link>
+      </p>
+    </div>
+  );
+};
 
 const LoginButton = () => {
   const { pending } = useFormStatus();
@@ -30,75 +127,6 @@ const LoginButton = () => {
     >
       Iniciar sesión
     </Button>
-  );
-};
-
-const LoginForm = () => {
-  const [isVisible, setIsVisible] = useState(false);
-  const [resMessage, dispatch] = useFormState(loginUser, undefined);
-  const router = useRouter();
-
-  // Redirect if login was successful
-  if (resMessage?.status === 200) router.push("/profile");
-
-  const toggleVisibility = () => setIsVisible(!isVisible);
-
-  return (
-    <div className="z-10 bg-slate-200/80 backdrop-blur-lg dark:bg-slate-800 md:shadow-lg dark:shadow-gray-900 md:rounded-xl p-6 w-full md:max-w-md md:border-solid md:border-2 md:border-slate-200 dark:md:border-slate-600">
-      <h1 className="mb-12 [text-shadow:_2px_2px_5px_rgb(0_0_0_/_25%)] font-black text-neutral-700 dark:text-slate-300 text-3xl text-center ">
-        Iniciar sesión
-      </h1>
-
-      <form className="flex flex-col items-center" action={dispatch}>
-        <Input
-          isRequired
-          type="email"
-          label="Email"
-          name="email"
-          labelPlacement="outside"
-          placeholder="xx-xxxxxxx@usb.ve"
-          description="Ingresa tu correo USB"
-          isInvalid={resMessage?.message === "El usuario no existe"}
-          errorMessage={
-            resMessage?.message === "El usuario no existe" &&
-            "El usuario no existe, ¿quisieras registrarte? "
-          }
-          variant="faded"
-          className="mb-8"
-        />
-        <Input
-          isRequired
-          name="password"
-          label="Contraseña"
-          labelPlacement="outside"
-          placeholder="Ingresa tu contraseña"
-          endContent={
-            <button
-              className="focus:outline-none"
-              type="button"
-              onClick={toggleVisibility}
-            >
-              {isVisible ? <EyeIcon /> : <EyeSlashIcon />}
-            </button>
-          }
-          type={isVisible ? "text" : "password"}
-          isInvalid={resMessage?.message === "Contraseña Incorrecta"}
-          errorMessage={
-            resMessage?.message === "Contraseña Incorrecta" &&
-            "La contraseña es incorrecta"
-          }
-          variant="faded"
-        />
-
-        <LoginButton />
-      </form>
-      <p className="mt-4 text-sm text-gray-600 text-center">
-        ¿Aún no estás registrado?{" "}
-        <Link href="/register" className="text-blue-500">
-          Regístrate
-        </Link>
-      </p>
-    </div>
   );
 };
 
