acb030e6f7ec24d531a141a728ce9439e7e02746
ChuyB <bovea3359@gmail.com>
2024-02-28 17:56:55 -0400

    feat: adds data-theme to html tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" data-theme="light">
       <body
         className={`${inter.className} light text-foreground bg-background`}
       >
