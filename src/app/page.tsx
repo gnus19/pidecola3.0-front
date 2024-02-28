415a8ffcea9b995d6c4cd2dc83ae5642f9656b2e
ChuyB <bovea3359@gmail.com>
2024-02-28 17:58:35 -0400

    refactor: hero section and logos section are moved to their own component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,16 @@
-import Image from "next/image";
 import Header from "@/components/Header";
-import usb from "../assets/usb.png";
-import fce from "../assets/logo_federacion.png";
-import CallToActionButton from "@/components/CallToActionButton";
+import LogosSection from "@/sections/LogosSection";
+import HeroSection from "@/sections/HeroSection";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-neutral-50">
+    <div className="flex flex-col items-center min-h-screen bg-neutral-50 dark:bg-gray-800">
       <Header />
 
-      <section className="relative flex flex-col gap-8 md:gap-24 justify-start items-center px-10 md:px-40 pt-24 pb-10 min-h-[60vh] max-h-[70vh] text-center overflow-hidden">
-        <h1 className="z-20 text-3xl md:text-5xl leading-tight font-black text-blue-950 animate-fade-up drop-shadow-md ">
-          {"En cada viaje compartido, tejemos historias, creamos lazos y "}
-          <span className="bg-clip-text text-transparent bg-gradient-to-br from-yellow-400  to-orange-500 animate-pulse animate-duration-[3000ms]">
-            forjamos un camino juntos{" "}
-          </span>
-        </h1>
-
-        <CallToActionButton />
-      </section>
-
-      {/* <section className="flex flex-row mt-10 px-52 justify-between items-center">
-        <Image
-          src={usb}
-          alt="Logo de la Universidad Simón Bolívar"
-          height={150}
-        />
-        <Image
-          src={fce}
-          alt="Logo de la Federación de Centros de Estudiantes de la Universidad Simón Bolívar"
-          height={150}
-        />
-      </section> */}
+      <main className="flex flex-col grow items-center px-10 md:px-24 lg:px-32 xl:px-40 max-w-7xl">
+        <HeroSection />
+        <LogosSection />
+      </main>
     </div>
   );
 }
