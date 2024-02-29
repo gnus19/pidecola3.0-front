bbfed7d5cf4e91bb70f0b00e79366489296d31d4
ChuyB <bovea3359@gmail.com>
2024-02-29 11:22:59 -0400

    refactor: header on main page renders logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,8 @@ import Link from "next/link";
 import logo from "../assets/logo.png";
 import { useEffect, useState } from "react";
 import UserActions from "./UserActions";
-import { usePathname } from "next/navigation";
 
 const Header = () => {
-  const pathname = usePathname();
-  const isHomePage = pathname === "/";
   const [isTop, setIsTop] = useState(true);
 
   useEffect(() => {
@@ -25,15 +22,15 @@ const Header = () => {
   return (
     <header
       className={`z-50 sticky top-0 min-h-14 py-2 px-4 sm:px-10 flex justify-between sm:justify-end items-center w-full dark:bg-gray-800 transition ease-in-out duration-300 
-        ${isTop
-          ? "bg-neutral-50"
-          : "shadow-md backdrop-blur-sm bg-neutral-200/80 dark:bg-gray-900/80"
+        ${
+          isTop
+            ? "bg-neutral-50"
+            : "shadow-md backdrop-blur-sm bg-neutral-200/80 dark:bg-gray-900/80"
         }`}
     >
       <Link
         href={"/"}
-        className={`${isHomePage && isTop ? "opacity-0 pointer-events-none" : "opacity-100"} sm:absolute w-fit right-0 left-0 sm:mx-auto text-blue-600 transition`}
-        aria-hidden={isHomePage && isTop}
+        className="ml-8 sm:absolute w-fit right-0 left-0 sm:mx-auto text-blue-600 transition"
       >
         <Image src={logo} alt="Logo del PideCola" height={40} width={40} />
       </Link>
