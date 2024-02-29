78488a0d131e3c0a41c0e253857c2adceff6e33c
ChuyB <bovea3359@gmail.com>
2024-02-29 11:26:48 -0400

    refactor: now renders an svg image and not the app logo

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,29 +1,32 @@
 import Image from "next/image";
-import logo from "@/assets/logo.png";
+import carpool from "@/assets/carpool.svg";
 import CallToActionButton from "@/components/CallToActionButton";
 
 const HeroSection = () => {
   return (
     <section
       className={`
-          relative flex flex-col lg:flex-row
-          justify-start lg:justify-between items-center gap-10 sm:gap-24 lg:gap-0
+          relative flex flex-col items-center justify-center
           w-full text-center mt-10 pb-14 md:pb-24
           border-b-1 border-slate-300
         `}
     >
-      <div className="w-full md:max-w-sm lg:max-w-md xl:max-w-lg">
-        <h1 className="z-20 mb-20 sm:mb-10 text-3xl leading-tight font-black text-blue-950 dark:text-orange-50 animate-fade-up drop-shadow-md">
+      <div className="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 place-items-center gap-8 lg:gap-20 mb-12 md:mb-24">
+        <h1 className="z-20 text-3xl leading-tight font-black text-blue-950 dark:text-orange-50 animate-fade-right drop-shadow-md">
           {"En cada viaje compartido, tejemos historias, creamos lazos y "}
           <span className="bg-clip-text text-transparent bg-gradient-to-br from-yellow-400  to-orange-500 animate-pulse animate-duration-[3000ms]">
             forjamos un camino juntos{" "}
           </span>
         </h1>
 
-        <CallToActionButton />
+        <Image
+          src={carpool}
+          alt="Una imágen vectorizada de dos personas junto a un vehículo azul"
+          className="animate-fade-left"
+        />
       </div>
 
-      <Image src={logo} alt="Logo del PideCola USB" className="w-60 lg:w-72" />
+      <CallToActionButton className="" />
     </section>
   );
 };
