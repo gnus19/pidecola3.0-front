ccb4b4507c87564769681b85771f27209d71543b
ChuyB <bovea3359@gmail.com>
2024-02-29 11:22:37 -0400

    refactor: cta button now accepts className props

--- src/components/CallToActionButton.tsx
+++ src/components/CallToActionButton.tsx
@@ -3,13 +3,13 @@
 import { Button } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
-const CallToActionButton = () => {
+const CallToActionButton = ({ className = "" }: { className?: string }) => {
   const router = useRouter();
   return (
     <Button
       size="lg"
       radius="full"
-      className="bg-gradient-to-br from-yellow-400 via-orange-500 to-red-500 font-bold text-white animate-bounce animate-duration-[5000ms]"
+      className={`${className} bg-gradient-to-br from-yellow-400 via-orange-500 to-red-500 font-bold text-white animate-bounce animate-duration-[5000ms]`}
       onClick={() => router.push("/login")}
     >
       ¡Únete ahora!
