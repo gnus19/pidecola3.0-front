de24a9a05b6244b106ea2dfb1b90e2f0b8dbbb21
ChuyB <bovea3359@gmail.com>
2024-03-05 20:53:32 -0400

    deletes uneeded jwt generator function

--- src/lib/services/apiKey.ts
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-const API_SECRET = process.env.API_SECRET;
-const AUDIENCE = process.env.AUDIENCE;
-
-export default function getApiToken() {
-  return jwt.sign({ iss: "client", aud: AUDIENCE }, API_SECRET);
-}
