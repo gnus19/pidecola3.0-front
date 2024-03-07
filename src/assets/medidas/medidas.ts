38a9ea69a770fc2450106f1b7f41c380a348e2ce
KevinJOH <13-11007@usb.ve>
2024-03-07 13:18:23 -0400

    Correccion de tildes

--- src/assets/medidas/medidas.ts
+++ src/assets/medidas/medidas.ts
@@ -1,20 +1,20 @@
 export const medidasBio = [
   "Usa el tapabocas",
-  "Desinfecta tus manos antes de abordar el vehiculo",
-  "Si estas contagiado con algun virus, evita contagiar a los demas",
-  "Si eres conductor, no olvides tu alcohol dentro del vehiculo"
+  "Desinfecta tus manos antes de abordar el vehículo",
+  "Si estas contagiado con algún virus, evita contagiar a los demás",
+  "Si eres conductor, no olvides tener alcohol dentro del vehículo"
 ]
 
 export const medidasUsuario = [
-  "Usa el cinturon de seguridad",
-  "Checa el vehiculo antes de bajarte, para evitar que olvides algo dentro de el.",
-  "Respeta las pertenencias de los demas usuarios",
-  "No ingieras alimentos dentro del vehiculo si no te han dado la autorizacion"
+  "Usa el cinturón de seguridad",
+  "Checa el vehículo antes de bajarte, para evitar que olvides algo dentro de él.",
+  "Respeta las pertenencias de los demás usuarios",
+  "No ingieras alimentos dentro del vehículo si no te han dado la autorización"
 ]
 
 export const medidasConductor = [
-  "Evita exceder los limites de velocidad",
-  "No uses el telefono mientras manejes",
-  "Usa el cinturon de seguridad",
-  "Respeta las pertenencias de los demas usuarios",
+  "Evita exceder los límites de velocidad",
+  "No uses el teléfono celular mientras manejes",
+  "Usa el cinturón de seguridad",
+  "Respeta las pertenencias de los demás usuarios",
 ]
