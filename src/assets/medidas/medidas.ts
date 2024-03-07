6f4d58c2749dc2b3c108e0d7c1209f0ba2c33d01
KevinJOH <13-11007@usb.ve>
2024-03-07 11:14:52 -0400

    Medidas

---
+++ src/assets/medidas/medidas.ts
@@ -0,0 +1,20 @@
+export const medidasBio = [
+  "Usa el tapabocas",
+  "Desinfecta tus manos antes de abordar el vehiculo",
+  "Si estas contagiado con algun virus, evita contagiar a los demas",
+  "Si eres conductor, no olvides tu alcohol dentro del vehiculo"
+]
+
+export const medidasUsuario = [
+  "Usa el cinturon de seguridad",
+  "Checa el vehiculo antes de bajarte, para evitar que olvides algo dentro de el.",
+  "Respeta las pertenencias de los demas usuarios",
+  "No ingieras alimentos dentro del vehiculo si no te han dado la autorizacion"
+]
+
+export const medidasConductor = [
+  "Evita exceder los limites de velocidad",
+  "No uses el telefono mientras manejes",
+  "Usa el cinturon de seguridad",
+  "Respeta las pertenencias de los demas usuarios",
+]
